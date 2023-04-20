0af823eba3d80284f7953a13d7d4dd943664e577
Damir Kambarov <damir.kambarov@qualixdev.com>
2023-04-20 19:48:42 +0500

    django, flake8 installed

---
+++ project/settings/__init__.py
@@ -0,0 +1,6 @@
+from .apps import *
+from .auth import *
+from .base import *
+from .database import *
+from .middleware import *
+from .templates import *
---
+++ project/settings/base.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = os.environ.get('DEBUG')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+
+ROOT_URLCONF = 'project.urls'
+
+WSGI_APPLICATION = 'project.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
